6273bd76934a4c3cd573289020e71cfed11b59f8
aadiG17 <122ad0022@iiitk.ac.in>
2025-10-09 14:52:29 +0530

    configuration

---
+++ utils/config.py
@@ -0,0 +1,6 @@
+# Centralized configuration
+DATA_PATH = "data/"
+MODEL_PATH = "models/"
+OUTPUT_PATH = "outputs/"
+SLM_MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+DIFFUSION_MODEL_NAME = "runwayml/stable-diffusion-v1-5"
